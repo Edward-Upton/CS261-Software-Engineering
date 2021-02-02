6253427b2d2469579c093d3964039c1bdab3eb8b
Aakash Kc <aakashkcx@gmail.com>
2021-02-02 01:25:15 +0000

    working socket.io connection to server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import socketIOClient from 'socket.io-client';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
 import { Button } from "@material-ui/core";
 
-const ENDPOINT = "127.0.0.1"
-
 function App() {
-  const [response, setResponse] = useState("no response yet");
-  const [data, setData] = useState<string[]>([]);
-  const [count, setCount] = useState<number>(0);
-  const [users, setUsers] = useState<string[]>([]);
+  // const [response, setResponse] = useState("no response yet");
+  // const [data, setData] = useState<string[]>([]);
+  // const [count, setCount] = useState<number>(0);
+  // const [users, setUsers] = useState<string[]>([]);
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT)
+    const socket = io("http://localhost:5000");
 
-    socket.on("message", (data: string) => {
-      console.log(data)
-    });
+    // socket.on("message", (data: string) => {
+    //   console.log(data);
+    // });
 
-    socket.on("count", (data: { newCount: number }) => {
-      console.log("new count received")
-      setCount(data.newCount)
-    })
+    // socket.on("count", (data: { newCount: number }) => {
+    //   console.log("new count received");
+    //   setCount(data.newCount);
+    // });
 
-    socket.on("usersUpdate", (data: { newUsers: string[] }) => {
-      console.log("new users received");
-      setUsers(data.newUsers);
-    })
-  }, [])
+    // socket.on("usersUpdate", (data: { newUsers: string[] }) => {
+    //   console.log("new users received");
+    //   setUsers(data.newUsers);
+    // });
+  }, []);
 
   return (
     <div className="App">
