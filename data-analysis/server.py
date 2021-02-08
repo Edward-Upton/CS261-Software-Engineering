66246c97928f0b063f2823171c14fd427dbecdca
Edward Upton <ed.upton@outlook.com>
2021-02-08 12:43:20 +0000

    Added axois to server

--- data-analysis/server.py
+++ data-analysis/server.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return "WORKING"
 
+@app.route('/emoji', methods=['POST'])
+def emoji():
+    data = request.json
+    newValue = data.value
+    field = data.field
+
+    
+    return data
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=4000, debug=True)
