51c0d9515ca77150b3f90df50b3eaf099c092d9b
Edward Upton <ed.upton@outlook.com>
2021-05-13 12:56:50 +0100

    Update config.ts

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,7 @@ export const PORT = 5000;
 
 /** URI of remote MongoDB database. */
 export const DB_URI =
-  "mongodb+srv://newUser:O1MHlEHLB5Zp0S83@cs261.h465i.mongodb.net/cs261?retryWrites=true&w=majority";
+  "mongodb+srv://<username>:<password>@cs261.h465i.mongodb.net/cs261?retryWrites=true&w=majority";
 /** Database config. */
 export const DB_OPTIONS = {
   useCreateIndex: true,
